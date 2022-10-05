099484c721390dfe0660a1e0443ec9b539d752be
anismaneri <anismaneri007@gmail.com>
2022-10-05 14:42:11 +0530

    Create assignment1.js
    Create assignment1.js

---
+++ assignment1.js
@@ -0,0 +1,9 @@
+console.log("Hello");
+console.log("My Name is Anis Maneri");
+console.log("My Hobbies 1) Traveling, 2) Cricket 3) Reading ");
+console.error("Personal Strength");
+console.log("1) Hard Work 2) Positive Thinker 3) Creativity");
+console.warn("Weakness");
+console.log("1) English Communication 2) Overthinking 3) Impatience");
+console.log(30+50);
+console.log(49*479);
