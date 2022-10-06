58cdb3f175e5be556dc1ae07fa311ea857860560
anismaneri <anismaneri007@gmail.com>
2022-10-06 18:28:42 +0530

    swap_variable.js
    swap_variable.js

---
+++ 01_variable.js
@@ -0,0 +1,11 @@
+var my_apple; // variable declaration
+my_apple ="strawberry";// variable initialization
+
+var my_name = "Anis Babu Maneri";//variable declaration and initialization on the same line
+my_name = "Anis Babasaheb Maneri ";
+console.log(my_name);
+console.log(my_apple);
+
+my_apple = "200";
+console.log(my_apple);
+  
