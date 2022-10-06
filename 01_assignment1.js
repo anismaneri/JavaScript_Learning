a37dc6aad3e93ad7708ee24e9537ce09fe479284
anismaneri <anismaneri007@gmail.com>
2022-10-06 22:50:19 +0530

    Create01_assignment.js
    Create01_assignment.js

---
+++ 01_assignment1.js
@@ -0,0 +1,19 @@
+var myFullName;
+myFullName="Anis Maneri";
+console.log(myFullName);
+myFullName="Anis Babu Maneri";
+console.log(myFullName);
+var myLove="I Love you Js";
+console.log(myLove);
+var firstName="Anis", MiddleName="Babu", LastName="Maneri", age="30", address="At post Asangi, Tq Asangi, Dist Sangli"
+pin_code="416412", gender="Male",is_Married="Unmarried";
+console.log(firstName);
+console.log(MiddleName);
+console.log( LastName);
+console.log(age);
+console.log(address);
+console.log(pin_code);
+console.log(gender);
+console.log(is_Married);
+
+
