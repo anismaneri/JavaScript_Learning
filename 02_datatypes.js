564570cef235c3373b292518c5730bebf3b2a6bb
anismaneri <anismaneri007@gmail.com>
2022-10-07 22:31:56 +0530

    create datatypes and function.js
    create datatypes and function.js

---
+++ 02_datatypes.js
@@ -0,0 +1,25 @@
+var p_name = "prasant";
+var age = 22;
+var city = 'pune';
+var married_status = false;
+
+var value_type;
+
+var value_type = typeof "Anis";
+console.log(typeof "Anis");
+console.log(typeof 31);
+console.log(typeof false);
+console.log("==================");
+console.log(typeof p_name);
+console.log(typeof age);
+console.log(typeof married_status);
+
+console.log("==================");
+var my_project_name;
+console.log( my_project_name);
+console.log("==================");
+console.log(typeof  my_project_name);
+ 
+var my_properties = null;
+console.log(typeof  my_properties);
+ 
