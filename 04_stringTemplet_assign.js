586bd41c3b8096e2cba82ac72fe0de42102f0495
anismaneri <anismaneri007@gmail.com>
2022-10-12 18:16:10 +0530

    04_stringTemplet_assign.js
    04_stringTemplet_assign.js

---
+++ 04_stringTemplet_assign.js
@@ -0,0 +1,20 @@
+console.log("======Q.1============");
+var greet ='My dream company :-"Atos"';
+console.log(greet);
+
+console.log("======Q.2============");
+console.log("____My Hobbies_________");
+var greet ='Hobby-1=Dancing';
+console.log(greet);
+var greet ='Hobby-2=Watching Movie';
+console.log(greet);
+var greet ='Hobby-3=Listing music';
+console.log(greet);
+var greet ='Hobby-4=Reading Books';
+console.log(greet);
+
+console.log("____concat string_________");
+var fName= "1.Dancing, 2.Watching movie";
+var lName= " 3.Listing music 4.Reading Books";
+var result = fName.concat(lName); // ViratKohli
+console.log(result);
